d691842765578e8335b070269e76033841b3bf11
shing.rcpet <shing@rcpet.ntnu.edu.tw>
2023-11-02 09:23:00 +0800

    test actionsUpdate dotnet.yml

--- github_actions/WeatherForecast.cs
+++ github_actions/WeatherForecast.cs
@@ -9,5 +9,6 @@ namespace github_actions
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+        public string? MyName { get; set; }
     }
 }
